98beec32f95b91282644d6e1a6cc2f3e6639ee2e
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-09 17:17:45 -0600

    Test 3

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
-                  "mongodb://localhost/personal-api");
+mongoose.connect(process.env.MONGODB_URI || 
+	process.env.MONGOLAB_URI || 
+	process.env.MONGOHQ_URL || 
+	"mongodb://localhost/personal-api”);
 
 
 module.exports.Movies = require("./movies.js");
